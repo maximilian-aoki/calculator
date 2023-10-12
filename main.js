55692ab15ab6246053b23ab3086825799811d5f8
Maximilian Aoki <135271252+maximilian-aoki@users.noreply.github.com>
2023-10-12 01:05:40 -0400

    Initialize calculator object with methods and attr

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log("Hello World!");
+// set up calculator properties and methods to be accessed and manipulated
+const calc = {
+  a: null,
+  b: null,
+  currentOperand: undefined,
+  currentOperator: undefined,
+  methods: {
+    '+': (num1, num2) => num1 + num2,
+    '-': (num1, num2) => num1 - num2,
+    'x': (num1, num2) => num1 * num2,
+    '/': (num1, num2) => num1 / num2,
+    '%': (num) => num / 100,
+    'sign': (num) => - num,
+  },
+};
